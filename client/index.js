3123a33237b9d84897d88d93e714e79d3aa80501
Aral Balkan <mail@ar.al>
2019-10-28 14:21:18 +0000

    Revise and improve the Firefox localhost timeout/disconnection fix

--- client/index.js
+++ client/index.js
@@ -5,21 +5,44 @@ var on = require('sendevent')
 
 var token
 
+function reloadPage() {
+  // Firefox has a quirk where it times out and disconnects an EventSource
+  // connection when the host (not hostname) is “localhost” and it does
+  // a regular reload from memory cache. Instead, we must do a forced reload from
+  // the server and also flag it so that sendevent doesn’t also do
+  // a forced reload just to be sure (and thereby adding an unnecessary reload).
+  // Also, when we do a forced reload, Firefox loses the current scroll position
+  // so we must save that so checking for the existence of the object that contains
+  // the location to restore is how we know that we have asked for a force reload.
+  if (navigator.userAgent.includes('Firefox') && document.location.host === 'localhost') {
+    console.log('[instant] Firefox running on host localhost. Doing full reload.')
+    window.locationToRestoreAfterForcedReload = {
+      x: window.scrollX,
+      y: window.scrollY
+    }
+    location.reload(true)
+  } else {
+    // In every other case, we can just do a regular reload.
+    console.log('[instant] Not Firefox running on host location. Doing regular reload.')
+    location.reload()
+  }
+}
+
 on('/instant/events', function(ev) {
   if (ev.token) {
     if (!token) token = ev.token
-    if (token != ev.token) return location.reload(true)
+    if (token != ev.token) return reloadPage()
   }
 
   // reload page if it contains an element with the given class name
   if (ev.className) {
-    if (find.byClass(ev.className)) location.reload(true)
+    if (find.byClass(ev.className)) reloadPage()
     return
   }
 
   // reload page if it contains an element that matches the given selector
   if (ev.selector) {
-    if (find.bySelector(ev.selector)) location.reload(true)
+    if (find.bySelector(ev.selector)) reloadPage()
     return
   }
 
@@ -28,7 +51,7 @@ on('/instant/events', function(ev) {
 
   // reload the page
   if (url.href == location.href) {
-    location.reload(true)
+    reloadPage()
     return
   }
 
@@ -38,5 +61,5 @@ on('/instant/events', function(ev) {
 
   // look for a script
   el = find.byURL('script', 'src', url)
-  if (el) location.reload(true)
+  if (el) reloadPage()
 })
